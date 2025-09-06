3d405e43b8a6ac3d02b112e52ecfd0e70bbf7782
p04ka <merklondon90@gmail.com>
2025-09-06 15:31:15 +0300

    do basic Rest API

--- go.mod
+++ go.mod
@@ -2,11 +2,13 @@ module github.com/P04KA/API.git
 
 go 1.25.1
 
-require github.com/gofiber/fiber/v2 v2.52.9
+require (
+	github.com/gofiber/fiber/v2 v2.52.9
+	github.com/google/uuid v1.6.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.2.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Age     string `json:"age"`
+	Country string `json:"country"`
+}
+
+var users = map[string]User{}
+
+func handler(c *fiber.Ctx) error {
+	return c.SendString("Test")
+}
+
+func getHandler(c *fiber.Ctx) error {
+	id := c.Params("id")
+	user := users[id]
+	if _, err := uuid.Parse(id); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+	if _, err := users[id]; !err {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Пользователя нет"})
+	}
+	return c.JSON(user)
+}
+
+func postHandler(c *fiber.Ctx) error {
+	var user User
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+	}
+	newID := uuid.New().String()
+	user.ID = newID
+	users[newID] = user
+
+	return c.JSON(fiber.Map{
+		"message": "Пользователь cоздан",
+		"user":    user,
+	})
+}
+
+func deleteHandler(c *fiber.Ctx) error {
+	id := c.Params("id")
+	if _, err := users[id]; !err {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Пользователя не существует"})
+	}
+	delete(users, id)
+	return c.JSON(fiber.Map{
+		"message": "Пользователь удален",
+		"id":      id,
+	})
+}
+func putHandler(c *fiber.Ctx) error {
+	var user User
+	id := c.Params("id")
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": err.Error()})
+	}
+	user.ID = id
+	users[id] = user
+
+	return c.JSON(fiber.Map{
+		"message": "Пользователь обновлен",
+		"user":    user,
+	})
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", handler)
+
+	app.Get("/user/:id", getHandler)
+
+	app.Post("/user", postHandler)
+
+	app.Put("/user/:id", putHandler)
+
+	app.Delete("/user/:id", deleteHandler)
+	if err := app.Listen(":3000"); err != nil {
+		os.Exit(1)
+	}
+	app.Listen(":3000")
+
+}
